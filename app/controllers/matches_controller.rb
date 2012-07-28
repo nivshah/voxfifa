c5b14c29d527573d8a4554273264d939b3042f95
Niv Shah <nivshah@gmail.com>
2012-07-28 15:00:31 -0400

    Added matches

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,3 +1,67 @@
 class MatchesController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+    @matches = Match.all
+  end
+
+  def new
+    @match = Match.new
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render :json => @post }
+    end
+  end
+
+  def create
+    @match = Match.new(params[:match])
+
+    respond_to do |format|
+      if @match.save
+        format.html { redirect_to(@match,
+                      :notice => 'Match was successfully created.') }
+        format.json { render :json => @match,
+                      :status => :created, :location => @match }
+      else
+        format.html { render :action => "new" }
+        format.json { render :json => @match.errors,
+                      :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+    @match = Match.find(params[:id])
+  end
+
+  def edit
+    @match = Match.find(params[:id])
+  end
+
+  def update
+    @match = Match.find(params[:id])
+
+    respond_to do |format|
+      if @match.update_attributes(params[:match])
+        format.html  { redirect_to(@match,
+                      :notice => 'Match was successfully updated.') }
+        format.json  { head :no_content }
+      else
+        format.html  { render :action => "edit" }
+        format.json  { render :json => @match.errors,
+                      :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @match = Match.find(params[:id])
+    @match.destroy
+
+    respond_to do |format|
+      format.html { redirect_to matches_url }
+      format.json { head :no_content }
+    end
+  end
 
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,6 @@ class Player < ActiveRecord::Base
   attr_accessible :name
 
   validates :name, :presence => true
+  has_many :home_matches, :class_name => "Match", :foreign_key => "home_player_id"
+  has_many :away_matches, :class_name => "Match", :foreign_key => "away_player_id"
 end
