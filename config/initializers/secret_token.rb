940e3f73dccccef0e5b6b41e4815f1d1b452ed05
Niv Shah <nivshah@gmail.com>
2012-07-28 00:11:02 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Voxfifa::Application.config.secret_token = '4f205d8e688ae1ea546474c0ccd984c6f5984181496d942f6d6af6976844263e191b4a6889cf14da6cb9bbf040c4f7ebee3e95b8478e585386d0a07d81033efa'
