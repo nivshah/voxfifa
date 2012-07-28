6b96d56ea29d7147b7c6c79f213f856eadd587a6
Niv Shah <nivshah@gmail.com>
2012-07-28 14:42:52 -0400

    Err adding attr_accessible back

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,6 +2,8 @@ class Match < ActiveRecord::Base
   has_one :home_player, :foreign_key => 'home_player_id', :class_name => 'Player'
   has_one :away_player, :foreign_key => 'away_player_id', :class_name => 'Player'
 
+  attr_accessible :home_player_id, :home_team, :away_player_id, :away_team, :home_score, :away_score
+
   validates :home_player, :presence => true
   validates :away_player, :presence => true
   validates :home_team, :presence => true
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,5 @@
 class Player < ActiveRecord::Base
+  attr_accessible :name
+
   validates :name, :presence => true
 end
