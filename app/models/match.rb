ca3aa38c0f12d650fb0f34f3b2953c8e2660d77e
Niv Shah <nivshah@gmail.com>
2012-07-28 14:01:43 -0400

    Validating the presence of certain values

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,4 +3,11 @@ class Match < ActiveRecord::Base
   has_one :away_player, :foreign_key => 'away_player_id', :class_name => 'Player'
 
   attr_accessible :home_team, :away_team, :home_score, :away_score
+
+  validates :home_player, :presence => true
+  validates :away_player, :presence => true
+  validates :home_team, :presence => true
+  validates :away_team, :presence => true
+  validates :home_score, :presence => true
+  validates :away_score, :presence => true
 end
