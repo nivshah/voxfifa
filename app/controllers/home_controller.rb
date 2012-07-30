7df6c3aca56761fba5180ac89a99da8724503ea5
Niv Shah <nivshah@gmail.com>
2012-07-29 21:39:36 -0400

    Use bootstrap classes for tables properly

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def index
+    @standings = []
+    players = Player.all
+    players.each do |player|
+      @standings << Standing.new(player.id)
+    end
+
+    # sort by points descending
+    @standings.sort! { |a,b| b.points <=> a.points }
+
     @matches = Match.all
   end
 end
