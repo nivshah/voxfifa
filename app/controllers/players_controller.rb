9c3d157d5037d3821e5d6d4635055c351ac71ca5
Niv Shah <nivshah@gmail.com>
2012-10-23 10:18:00 -0400

    Nicer to not logged in types

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,5 @@
 class PlayersController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
   def index
     @players = Player.all
